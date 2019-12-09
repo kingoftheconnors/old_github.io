b61f4b003260369f4e58d4eaa50cf1661ae46239
kingoftheconnors <Connormcp777@gmail.com>
2019-12-09 17:36:34 -0500

    Added header box

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
   var scrolledY = $(window).scrollTop();
   $('.picture').css('background-position', 'left ' + ((scrolledY)) + 'px');
 });*/
+jQuery(document).ready(function(t) {
+  t(".level-bar-inner").css("width","0"),
+  t(window).on("load", function() { 
+    t(".level-bar-inner").each(function() {
+      var n=t(this).data("level");
+      t(this).animate({width:n},800)
+    })
+  })
+})
