936d9a4a5b15e81992ff2bfca52c3c278bf54902
kingoftheconnors <Connormcp777@gmail.com>
2019-12-10 12:56:54 -0500

    Added about me content

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
   $('.picture').css('background-position', 'left ' + ((scrolledY)) + 'px');
 });*/
 jQuery(document).ready(function(t) {
-  t(".level-bar-inner").css("width","0"),
+  t(".bar").css("width","0"),
   t(window).on("load", function() { 
-    t(".level-bar-inner").each(function() {
+    t(".bar").each(function() {
       var n=t(this).data("level");
       t(this).animate({width:n},800)
     })
